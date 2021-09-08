b52890b5d68c89bceaea33af6f61f02a513757d0
Jonathan Gonzalez <Jonathan.gonzalez@powerhrg.com>
2021-09-08 10:17:10 -0400

    commit tree 7415e50115dc2f8c1c4c75f584975b443b43d16b

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/students', to: 'students#index'
+  get '/students/grades', to: 'students#grades'
+  get '/students/highest-grade', to: 'students#highest_grade'
 end
+
+
+# for the first get request, after the "to:", you want to call on the correct method inside of the students_controller
+# or whatever controller you are working out of. 
